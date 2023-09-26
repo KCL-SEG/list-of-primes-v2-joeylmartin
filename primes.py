246c969b0969e93a1af673791bcbd08ce83330b9
iBlu_HD <jlmartin9000@gmail.com>
2023-09-26 10:58:42 +0100

    added prime finder

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+
+    if number_of_primes <= 0:
+        raise ValueError("Inappropriate argument!")
+
+
+    index = 1
+    while True:
+        index += 1
+        flag = True
+        for i in range(2, 1 + index // 2):
+            if index % i == 0:
+                flag = False
+                break
+                
+        if flag:
+            list.append(index)
+            if len(list) >= number_of_primes:
+                return list
+
+
+
+primes(5)
